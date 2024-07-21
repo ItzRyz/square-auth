4e21639ab837466beafd288b993c409da0d4fe63
ItzRyz <zetsura.official04@gmail.com>
2024-07-21 21:06:51 +0700

    Initial commit

--- src/main/kotlin/alyva/yuzari/squareauth/Main.kt
+++ src/main/kotlin/alyva/yuzari/squareauth/Main.kt
@@ -1,11 +1,14 @@
 package alyva.yuzari.squareauth
 
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 class Main : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     override fun onDisable() {
