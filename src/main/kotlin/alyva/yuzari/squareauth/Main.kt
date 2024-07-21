caa8594fff2dbe1a6546ad5a96b7d91f808edd6a
ItzRyz <zetsura.official04@gmail.com>
2024-07-21 21:01:13 +0700

    Initial commit

---
+++ src/main/kotlin/alyva/yuzari/squareauth/Main.kt
@@ -0,0 +1,14 @@
+package alyva.yuzari.squareauth
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
